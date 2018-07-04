48247073fbdf75b502f92af8921cafd68141b2e5
woniper <leekw3747@gmail.com>
2018-07-04 20:42:29 +0900

    도서 검색 후 서점 검색 modal 개발
    - 도서 검색 후 도서 선택 시 해당 도서를 보유하고 있는 오프라인 서점 조회
    - vue-js-modal 사용

--- bookup-front/src/components/book/BookList.vue
+++ bookup-front/src/components/book/BookList.vue
@@ -1,33 +1,46 @@
 <template>
-  <v-infinite-scroll :loading="loading" @bottom="nextPage"
-                     style="max-height: 80vh; overflow-y: scroll;">
+  <div>
+    <v-infinite-scroll :loading="loading" @bottom="nextPage"
+                       style="max-height: 80vh; overflow-y: scroll;">
 
-    <div class="container" v-for="book in content">
-      <div class="row" v-on:click="clickBook(book)">
-        <div class="col-md-2">
-          <a href="#">
-            <img class="img-book img-fluid rounded mb-3 mb-md-0" v-bind:src="book.image">
-          </a>
+      <div class="container" v-for="book in content" style="cursor: pointer;">
+        <div class="row" v-on:click="clickBook(book)">
+          <div class="col-md-2">
+            <a href="#">
+              <img class="img-book img-fluid rounded mb-3 mb-md-0" v-bind:src="book.image">
+            </a>
+          </div>
+          <div class="col-md-10">
+            <h3 class="book-title">{{ book.title }}</h3>
+            <p class="book-description">{{ book.description }}</p>
+            <p class="book-description">가격 : {{ book.price }} 원</p>
+          </div>
         </div>
-        <div class="col-md-10">
-          <h3 class="book-title">{{ book.title }}</h3>
-          <p class="book-description">{{ book.description }}</p>
-          <p class="book-description">가격 : {{ book.price }} 원</p>
-        </div>
+        <hr>
       </div>
-      <hr>
-    </div>
-  </v-infinite-scroll>
+    </v-infinite-scroll>
+
+    <modal name="storeModal" :scrollable="true" height="auto">
+      <div v-for="store in stores">
+        <a v-bind:href="store.href" target="_blank"> {{ store.storeName }} </a>
+      </div>
+    </modal>
+  </div>
+
 </template>
 
 <script>
   import Vue from 'vue'
   import InfiniteScroll from 'v-infinite-scroll'
   import 'v-infinite-scroll/dist/v-infinite-scroll.css'
+
+  // https://www.npmjs.com/package/vue-js-modal
+  import VModal from 'vue-js-modal'
   import api from '@/api'
   import StoreModel from '../../models/StoreModel'
 
   Vue.use(InfiniteScroll)
+  Vue.use(VModal)
 
   export default {
     name: 'search',
@@ -40,7 +53,8 @@
           page: 0,
           last: false
         },
-        content: []
+        content: [],
+        stores: []
       }
     },
 
@@ -79,17 +93,13 @@
       clickBook (book) {
         StoreModel.getStores(book.isbn).then(stores => {
           if (stores.length <= 0) {
+            this.stores = []
             alert('모든 서점에서 도서를 찾을 수 없습니다.')
             return
           }
 
-          let message = ''
-
-          stores.forEach(function (store) {
-            message += store.storeName + '\n'
-          })
-
-          alert(message)
+          this.stores = stores
+          this.$modal.show('storeModal')
         })
       }
     }
