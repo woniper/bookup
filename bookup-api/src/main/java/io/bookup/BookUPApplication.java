69bec686c8fbf3d632179d922accc0573d64a6ed
woniper <leekw3747@gmail.com>
2018-07-23 09:41:35 +0900

    Spring Boot Application 실행 시 pid 파일로 저장
    - 서버에 배포 후 애플리케이션 실행 후 shutdown 하기 위해 pid 파일로 저장

--- bookup-api/src/main/java/io/bookup/BookUPApplication.java
+++ bookup-api/src/main/java/io/bookup/BookUPApplication.java
@@ -1,7 +1,9 @@
 package io.bookup;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,7 +14,10 @@ import org.springframework.web.client.RestTemplate;
 public class BookUPApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BookUPApplication.class, args);
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(BookUPApplication.class)
+                .web(WebApplicationType.SERVLET);
+        builder.build().addListeners(new ApplicationPidFileWriter("./bin/shutdown.pid"));
+        builder.run();
     }
 
     @Bean
