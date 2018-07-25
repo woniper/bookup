5bbae88da2d56d5c2967548b0f0103a852c09b96
woniper <leekw3747@gmail.com>
2018-07-26 08:00:21 +0900

    travis 배포 시 압축된 앱파일 전송 후 실행
    - travis에서 빌드 성공 후 빌드된 파일 압축 후 scp로 remote에 파일 전송
    - remote는 다시 빌드하지 않고 전송받은 압축 파일만 실행함.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+openssl aes-256-cbc -K $encrypted_71566cf63c17_key -iv $encrypted_71566cf63c17_iv -in deploy_rsa.enc -out /tmp/deploy_rsa -d
+eval "$(ssh-agent -s)"
+chmod 0400 /tmp/deploy_rsa
+ssh-add /tmp/deploy_rsa
+
+scp -oStrictHostKeyChecking=no ../bookup-api/build/libs/bookup-api.jar $SSH_HOST:$SSH_HOME
+scp -oStrictHostKeyChecking=no ../bookup-front/app.zip $SSH_HOST:$SSH_HOME
+
 ssh -T -oStrictHostKeyChecking=no $SSH_HOST <<EOF
     cd $SSH_HOME
-    nohup ./deploy.sh &
+    ./run.sh
 EOF
